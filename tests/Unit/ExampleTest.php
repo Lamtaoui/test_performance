c34932e1623cb40c3eba2a2edac0fd29c8165b07
Lamtaoui <mlamtaoui1@myges.fr>
2021-04-04 20:52:04 +0200

    [Test]

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit;
 
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TodolistController;
 use Tests\TestCase;
+use App\Item;
+use App\Todolist;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
@@ -14,12 +17,22 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testIsValidNominal()
+    public TodoList $todolist;
+
+    public function __construct()
     {
-        $data = ['name'=>'','content'=>random_bytes(1001)];
+        $todolist=new Todolist('test','1');
+    }
+    public function todoIsValidExist()
+    {
+        $this->assertTrue(TodolistController::is_Valid());
+    }
+    public function itemIsValidContent()
+    {
+        $data = ['name'=>'az','content'=>random_bytes(1001)];
         $this->assertFalse(ItemController::is_Valid($data));
     }
-    public function testIsValidNameExist()
+    public function itemIsValidNameExist()
     {
         $data = ['name'=>'az','content'=>'aaaaaaaa'];
         $this->assertFalse(ItemController::is_Valid($data));
