f5efb63d80223d3d2d9a74289e364bff3f02eba1
Lamtaoui <77679054+Lamtaoui@users.noreply.github.com>
2021-07-18 19:20:38 +0200

    Modif routes

--- app/Http/Controllers/JurryController.php
+++ app/Http/Controllers/JurryController.php
@@ -3,104 +3,87 @@
 namespace App\Http\Controllers;
 
 use App\Item;
-use App\Jurry;
-use App\JurryModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Todolist;
+use App\Jurry;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use App\Services\PayUService\Exception;
 
 class JurryController extends Controller
 {
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/items';
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255|unique:items',
-            'content' => 'required|string|min:255',
-        ]);
-    }
-    public function index()
-    {
-        $jurries = DB::table('jurry')->get();
-        return view('jurries',compact('jurries',$jurries));
 
+    public function getWithLimit($id)
+    {
+        try {
+            $jurries = DB::table('jurries')->limit($id)->get();
+            return response()->json($jurries, 200);
+        } catch (\Exception $e) {
+
+            return response()->json($e, 400);
+        }
 
     }
-    public function create()
+    public function getAll()
     {
-        return view('item/create');
-    }
-    public function is_valid(array  $request){
-        $item2=Item::where('name',$request['name'])->first();
-        $valid=true;
-        if(!empty($item2)){
-            $valid=false;
-        }
-        if(strlen($request['content'])>1000){
-            $valid=false;
-        }
-        $todolist=Todolist::where('user_id',auth::id())->first();
-        $cp=Item::where('todolist_id',$todolist->id)->count();
-        if($cp==8){
-            //$this->sendMail();
-        }
-        if($cp>10) {
-            $valid=false;
-        }
-        $item=Item::where('todolist_id',$todolist->id)->latest('created_at')->first();
-        if(!empty($item)){
-        $current_timestamp = \Carbon\Carbon::now()->toDateTimeString();
-        $start = Carbon::parse($item->created_at);
-        $end = Carbon::parse($current_timestamp);
-        $diff=$end->diffInMinutes($start);
-        if($diff<30){
-            $valid=false;
-        }
-        }
-        return $valid;
-    }
-    public function add(Request $request)
-    {
+        try {
+            $jurries = DB::table('jurries')->get();
+            return response()->json($jurries, 200);
+        } catch (\Exception $e) {
 
-        $valid=$this->is_valid($request->all());
-        if($valid==false){
-            return redirect()->back() ->with('alert', 'the data not valid');
+            return response()->json($e, 400);
         }
-        $todolist=Todolist::where('user_id',auth::id())->first();
-                Item::create([
-                    'name' => $request->get('name'),
-                    'content' => $request->get('content'),
-                    'todolist_id' => $todolist->id,
-                ]);
-                return redirect()->route('items');
     }
-    public function delete($id)
+    public function destroy($id)
     {
-        Item::where('id',$id)->delete();
-        return redirect()->route('items');
+        try {
+            $jurry = Jurry::find($id);
+            $jurry->delete();
+            return response()->json("jurry supprime", 200);
+        } catch (\Exception $e) {
+
+            return response()->json($e, 400);
+        }
     }
-    public function edit($id)
+    public function lentFunction()
     {
-        $item=Item::where('id',$id)->first();
-        return view('item/update')->with('item', $item);
+        try {
+            $jurries = DB::table('jurries')->get();
+            $filterJurries[]=0;
+            foreach($jurries as $jury){
+                if($jury->role=='Expert.e'){
+                    array_push($filterJurries,$jury);
+                }
+            }
+            return response()->json( $filterJurries, 200);
+        } catch (\Exception $e) {
+
+            return response()->json($e, 400);
+        }
     }
-    public function update(Request $request)
+    public function store(Request $request)
     {
-        Item::where('id',$request->get('id'))->update(['name'=>$request->get('name'),'content'=>$request->get('content')]);
-        return redirect()->route('items');
+        try {
+            $jury=new Jurry;
+            $jury->aap = $request->get('aap');
+            $jury->role= $request->get('role');
+            $jury->idref= $request->get('idref');
+            $jury->orcid= $request->get('orcid');
+            $jury->prenom= $request->get('prenom');
+            $jury->nom= $request->get('nom');
+            $jury->genre= $request->get('genre');
+            $jury->iso_courntry= $request->get('iso_courntry');
+            $jury->pays= $request->get('pays');
+            $jury->code_affiliation= $request->get('code_affiliation');
+            $jury->affiliation= $request->get('affiliation');
+            $jury->save();
+            return response()->json( "jury created", 200);
+        } catch (\Exception $e) {
+
+            return response()->json($e, 400);
+        }
     }
+
 }
